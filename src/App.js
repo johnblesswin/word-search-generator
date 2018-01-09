0c35c66bcebe191de541a45d70a65901a61cbbc5
Robert Gulewicz <nayael@wp.pl>
2018-01-10 00:47:24 +0100

    Rearranged layout in the main container.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import './App.css';
-import { Icon, Header, Grid } from 'semantic-ui-react';
-import { Route, Switch, Redirect } from 'react-router-dom';
+import { Icon, Header, Grid, Menu } from 'semantic-ui-react';
+import { Route, Switch, Redirect, NavLink } from 'react-router-dom';
 
-import MainMenu from './modules/MainMenu';
+// app modules:
 import StartPage from './modules/StartPage';
 import SetupGrid from './modules/SetupGrid';
 import AddWords from './modules/AddWords';
@@ -13,6 +13,42 @@ import Print from './modules/Print';
 import Download from './modules/Download';
 
 class App extends React.Component {
+
+  mainMenu() {
+    return (
+      <Menu fluid vertical tabular size='massive'>
+        <Menu.Item as={NavLink} to='/setup-grid'>
+          <Icon name='grid layout' color='brown' />Setup grid
+        </Menu.Item>
+        <Menu.Item as={NavLink} to='/add-words'>
+          <Icon name='write' color='brown' />Add words
+        </Menu.Item>
+        <Menu.Item as={NavLink} to='/preview'>
+          <Icon name='eye' color='brown' />Preview
+        </Menu.Item>
+        <Menu.Item as={NavLink} to='/print'>
+          <Icon name='print' color='brown' />Print
+        </Menu.Item>
+        <Menu.Item as={NavLink} to='/download'>
+          <Icon name='file pdf outline' color='brown' />Download
+        </Menu.Item>
+      </Menu>
+    );
+  }
+
+  pageContent() {
+    return (
+      <Switch>
+        <Route exact path='/' component={StartPage} />
+        <Route path='/setup-grid' component={SetupGrid} />
+        <Route path='/add-words' component={AddWords} />
+        <Route path='/preview' component={Preview} />
+        <Route path='/print' component={Print} />
+        <Route path='/download' component={Download} />
+        <Route path="*" render={() => (<Redirect to="/" />)} />
+      </Switch>
+    );
+  }
 
   render() {
     return (
@@ -28,18 +64,10 @@ class App extends React.Component {
         </Header>
         <Grid>
           <Grid.Column width='4'>
-            <MainMenu />
+            {this.mainMenu()}
           </Grid.Column>
           <Grid.Column width='12'>
-            <Switch>
-              <Route exact path='/' component={StartPage} />
-              <Route path='/setup-grid' component={SetupGrid} />
-              <Route path='/add-words' component={AddWords} />
-              <Route path='/preview' component={Preview} />
-              <Route path='/print' component={Print} />
-              <Route path='/download' component={Download} />
-              <Route path="*" render={() => (<Redirect to="/" />)} />
-            </Switch>
+            {this.pageContent()}
           </Grid.Column>
         </Grid>
       </React.Fragment>
