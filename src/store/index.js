064517378af281830d22f9478fa5cbf9071c23f6
Robert Gulewicz <nayael@wp.pl>
2018-01-13 20:22:48 +0100

    Added thunk middleware.

--- src/store/index.js
+++ src/store/index.js
@@ -1,16 +1,16 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension/logOnlyInProduction';
+import thunk from 'redux-thunk';
 import rootReducer from '../reducers';
 import * as actionCreators from '../actions/wordListActions';
 import initialState from './initialState';
 
 export default function configureStore() {
-
-  const enhancer = window.__REDUX_DEVTOOLS_EXTENSION__ &&
-    window.__REDUX_DEVTOOLS_EXTENSION__({ actionCreators });
-
   return createStore(
     rootReducer,
     initialState,
-    enhancer
+    composeWithDevTools(applyMiddleware(
+      thunk
+    ))
   );
 }
