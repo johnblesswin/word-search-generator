c3f1337695c08053f4424b5f330432de636bc945
Robert Gulewicz <nayael@wp.pl>
2018-01-15 02:51:08 +0100

    Added new action creators.

--- src/actions/wordListActions.js
+++ src/actions/wordListActions.js
@@ -9,3 +9,15 @@ export function submitWord(word) {
 export function removeWord(word) {
   return {type: 'REMOVE_WORD', payload: {word}};
 }
+
+export function circleWord(word) {
+  return {type: 'CIRCLE_WORD', payload: {word}};
+}
+
+export function changeMaxLength(length) {
+  return {type: 'CHANGE_MAX_LENGTH', payload: {length}};
+}
+
+export function changeCharset(charset) {
+  return {type: 'CHANGE_CHARSET', payload: {charset}};
+}
