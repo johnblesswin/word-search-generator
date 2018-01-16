c8edb42790452f8cc3630c1760127381f94507ce
Robert Gulewicz <nayael@wp.pl>
2018-01-16 23:16:44 +0100

    Connected component to the store.

--- src/modules/Preview.js
+++ src/modules/Preview.js
@@ -1,6 +1,14 @@
 import React from 'react';
+import { bindActionCreators } from 'redux';
+import * as actions from '../actions';
+import { connect } from 'react-redux';
 
 class Preview extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.props.actions.requestPuzzle();
+  }
 
   render() {
     return (
@@ -10,4 +18,16 @@ class Preview extends React.Component {
 
 }
 
-export default Preview;
+function mapStateToProps(state, ownProps) {
+  return {
+    puzzle: state.puzzle
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    actions: bindActionCreators(actions, dispatch)
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Preview);
