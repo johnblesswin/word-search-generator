b9854768ee4dc351d658c63aab1c980e4b5c6313
Robert Gulewicz <nayael@wp.pl>
2018-01-18 01:49:53 +0100

    Reducer structure ready.

--- src/reducers/puzzleGeneratorReducer.js
+++ src/reducers/puzzleGeneratorReducer.js
@@ -2,8 +2,23 @@ import { puzzle as initialState } from '../store/initialState';
 
 export default function puzzleGeneratorReducer(state = initialState, action) {
   switch (action.type) {
-    case 'SET_GRID_SIZE':
+    case 'PUZZLE_IS_BEING_GENERATED':
+      return puzzleIsBeingGenerated(state);
+    case 'PUZZLE_GENERATED':
+      return puzzleGenerated(action.puzzle);
+    case 'ERROR_GENERATING_PUZZLE':
+      return errorGeneratingPuzzle(action.error);
     default:
       return state;
   }
+}
+
+function puzzleIsBeingGenerated(state) {
+
+}
+
+function puzzleGenerated(puzzle) {}
+
+function errorGeneratingPuzzle(error) {
+
 }
