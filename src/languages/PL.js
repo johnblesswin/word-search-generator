2dea73a84a60f5e885b4c6bbbbf4b5b57cee4b82
Robert Gulewicz <nayael@wp.pl>
2018-01-18 01:52:33 +0100

    Polish language (mock).

---
+++ src/languages/PL.js
@@ -0,0 +1,14 @@
+export const name = 'Polish';
+
+export const charset = ['a', 'ą', 'b', 'c', 'ć', 'd', 'e', 'ę', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'ł', 'm', 'n', 'o', 'ó', 'p', 'q', 'v', 'w', 'y', 'z'];
+
+export const messages = {
+  SUBMITTED_WORD_TOO_SHORT: "The word is too short.",
+  SUBMITTED_WORD_TOO_LONG: "The word is too long and won’t fit the board.",
+  SUBMITTED_WORD_HAS_INVALID_CHAR: "Invalid character: ",
+  SUBMITTED_WORD_HAS_INVALID_CHARS: "Invalid characters: ",
+  PROBLEM_WITH_SOME_WORDS: "Some words on the list don’t match the current grid and/or language settings and won’t be included.",
+  WORD_TOO_LONG: "This word is too long.",
+  WORD_HAS_INVALID_CHAR: "Invalid character: ",
+  WORD_HAS_INVALID_CHARS: "Invalid characters: "
+};
