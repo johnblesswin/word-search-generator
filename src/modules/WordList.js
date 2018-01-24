2b63de619f3d796ab8c4811a7846db1b009e35bb
Robert Gulewicz <nayael@wp.pl>
2018-01-24 04:39:22 +0100

    Switch from semantic ui to ant design

--- src/modules/WordList.js
+++ src/modules/WordList.js
@@ -1,73 +1,90 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import * as actions from '../actions';
-import { connect } from 'react-redux';
-import { Input, List } from 'semantic-ui-react';
+import { CSSTransitionGroup } from 'react-transition-group';
+import { Button, Input, Form } from 'antd';
 import WordListItem from './WordListItem';
 
 class WordList extends React.Component {
 
-  submitWord = () => {
-    this.props.actions.submitWord();
-  }
+    submitWord = () => {
+        this.props.actions.submitWord();
+    }
 
-  typeWord = (event, {value}) => {
-    this.props.actions.typeWord(value);
-  }
+    //typeWord = (event, {value}) => {
+    typeWord = (e) => {
+        this.props.actions.typeWord(e.target.value);
+    }
 
-  keyDown = ({keyCode}) => {
-    if (keyCode === 13) this.submitWord(); // Trigger submit on <Enter>
-  }
+    keyDown = ({keyCode}) => {
+        if (keyCode === 13) this.submitWord(); // Trigger submit on <Enter>
+    }
 
-  circleOutWord = (word) => {
-    //this.props.actions.circleOutWord(word);
-  }
+    circleOutWord = (word) => {
+        //this.props.actions.circleOutWord(word);
+    }
 
-  removeWord = (word) => {
-    this.props.actions.removeWord(word);
-  }
+    removeWord = (word) => {
+        this.props.actions.removeWord(word);
+    }
 
-  render() {
-    return (
-      <React.Fragment>
-        <Input
-          size='large'
-          action={{ onClick: this.submitWord,  color: 'brown', labelPosition: 'right', icon: 'add square', content: 'Add' }}
-          actionPosition='left'
-          placeholder='New word...'
-          onChange={this.typeWord}
-          onKeyDown={this.keyDown}
-          value={this.props.currentlyTyped.word}
-        />
-        <List celled>
-          {this.props.list.map(item =>
-            <WordListItem key={item.word} word={item.word} remove={this.removeWord} circleOut={this.circleOutWord} />
-          )}
-        </List>
-      </React.Fragment>
-    );
-  }
+    render() {
+
+        const items = this.props.list.map(item => {return <div key={item.word}>{item.word}</div>;});
+
+        return (
+            <React.Fragment>
+
+
+                <Form layout="inline">
+                    <Form.Item
+                        label=""
+                        validateStatus=""
+                    >
+                    <Input
+                        placeholder="Add word..."
+                        value={this.props.currentlyTyped.word}
+                        onPressEnter={this.keyDown}
+                        onChange={this.typeWord}
+                        size="large"
+                        id="add-word" />
+                    </Form.Item>
+                    <Button
+                        onClick={this.submitWord}
+                        type="primary"
+                        shape="circle"
+                        icon="plus"
+                        size="large"
+                    />
+                </Form>
+
+                {items}
+
+            </React.Fragment>
+        );
+    }
 
 }
 
 WordList.propTypes = {
-  actions: PropTypes.object.isRequired,
-  currentlyTyped: PropTypes.object.isRequired,
-  list: PropTypes.array.isRequired
+    actions: PropTypes.object.isRequired,
+    currentlyTyped: PropTypes.object.isRequired,
+    list: PropTypes.array.isRequired
 };
 
 function mapStateToProps(state, ownProps) {
-  return {
-    currentlyTyped: state.words.currentlyTyped,
-    list: state.words.list
-  };
+    return {
+        currentlyTyped: state.words.currentlyTyped,
+        list: state.words.list
+    };
 }
 
 function mapDispatchToProps(dispatch) {
-  return {
-    actions: bindActionCreators(actions, dispatch)
-  };
+    return {
+        actions: bindActionCreators(actions, dispatch)
+    };
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(WordList);
