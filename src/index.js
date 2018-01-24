6530bfa930257026a44938f67005335bee3771d7
Robert Gulewicz <nayael@wp.pl>
2018-01-24 04:40:37 +0100

    Remove semantic ui, add ant design

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import { render } from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import configureStore from './store';
 import { Provider } from 'react-redux';
-import 'semantic-ui-css/semantic.min.css';
 import './index.css';
 import App from './App';
 
