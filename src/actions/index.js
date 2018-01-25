15b7251740927fcd83f9bda5e6c5a58f6c2b3c6f
Robert Gulewicz <nayael@wp.pl>
2018-01-25 21:22:37 +0100

    Modify action creator

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,42 +1,40 @@
 import generatePuzzle from '../services/generatePuzzle';
 
 export function typeWord(word) {
-  return {type: 'TYPE_WORD', payload: {word}};
+ 	return {type: 'TYPE_WORD', payload: {word}};
 }
 
 export function submitWord() {
-  return {type: 'SUBMIT_WORD'};
+  	return {type: 'SUBMIT_WORD'};
 }
 
 export function removeWord(word) {
-  return {type: 'REMOVE_WORD', payload: {word}};
+	return {type: 'REMOVE_WORD', payload: {word}};
 }
 
 export function circleWord(word) {
-  return {type: 'CIRCLE_WORD', payload: {word}};
+	return {type: 'CIRCLE_WORD', payload: {word}};
 }
 
 export function setGridSize(size) {
-  return {type: 'SET_GRID_SIZE', payload: {size}};
+    return {type: 'SET_GRID_SIZE', payload: {size}};
 }
 
 export function switchLanguage(langCode) {
-  return {type: 'SWITCH_LANGUAGE', payload: {langCode}};
+    return {type: 'SWITCH_LANGUAGE', payload: {langCode}};
 }
 
 export function requestPuzzle() {
-  return (dispatch, getState) => {
-    if(getState().settings.grid.touched || getState().words.touched) {
-
-      dispatch({type: 'PUZZLE_IS_BEING_GENERATED'});
-      generatePuzzle(/* */)
-        .then(puzzle => dispatch({type: 'PUZZLE_GENERATED', payload: {puzzle}}))
-        .catch(error => dispatch({type: 'ERROR_GENERATING_PUZZLE', error}));
-
-    }
-  };
+    return (dispatch, getState) => {
+        if (getState().settings.grid.touched || getState().words.touched) {
+            dispatch({type: 'PUZZLE_GENERATION_PENDING'});
+            generatePuzzle()
+                .then(puzzle =>  dispatch({type: 'PUZZLE_GENERATION_COMPLETE', payload: {puzzle}}))
+                .catch(error => dispatch({type: 'PUZZLE_GENERATION_ERROR', error}));
+        }
+    };
 }
 
 export function requestAnotherPuzzle() {
-  return requestPuzzle();
+    return requestPuzzle();
 }
