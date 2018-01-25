b464cdb20bce22f025c9fe78113789fa9542036b
Robert Gulewicz <nayael@wp.pl>
2018-01-25 21:23:38 +0100

    Remove unnecessary mock

--- src/services/__mocks__/generateWordSearch.js
+++
@@ -1,8 +0,0 @@
-export default function generateWordSearch(resolveValue)
-{
-console.log('mock generateWordSearch');
-  return new Promise((resolve, reject) => {
-    resolve(resolveValue);
-  });
-
-}
