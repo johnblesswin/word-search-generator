a5a9ff14d25e8fb9a98bb4d4a098129c7d60ee9d
Robert Gulewicz <nayael@wp.pl>
2018-01-26 16:37:42 +0100

    Implement action type constants

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,36 +1,37 @@
+import * as types from './types';
 import generatePuzzle from '../services/generatePuzzle';
 
 export function typeWord(word) {
- 	return {type: 'TYPE_WORD', payload: {word}};
+ 	return {type: types.TYPE_WORD, payload: {word}};
 }
 
 export function submitWord() {
-  	return {type: 'SUBMIT_WORD'};
+  	return {type: types.SUBMIT_WORD};
 }
 
 export function removeWord(word) {
-	return {type: 'REMOVE_WORD', payload: {word}};
+	return {type: types.REMOVE_WORD, payload: {word}};
 }
 
 export function circleWord(word) {
-	return {type: 'CIRCLE_WORD', payload: {word}};
+	return {type: types.CIRCLE_WORD, payload: {word}};
 }
 
 export function setGridSize(size) {
-    return {type: 'SET_GRID_SIZE', payload: {size}};
+    return {type: types.SET_GRID_SIZE, payload: {size}};
 }
 
 export function switchLanguage(langCode) {
-    return {type: 'SWITCH_LANGUAGE', payload: {langCode}};
+    return {type: types.SWITCH_LANGUAGE, payload: {langCode}};
 }
 
 export function requestPuzzle() {
     return (dispatch, getState) => {
         if (getState().settings.grid.touched || getState().words.touched) {
-            dispatch({type: 'PUZZLE_GENERATION_PENDING'});
+            dispatch({type: types.PUZZLE_GENERATION_PENDING});
             generatePuzzle()
-                .then(puzzle =>  dispatch({type: 'PUZZLE_GENERATION_COMPLETE', payload: {puzzle}}))
-                .catch(error => dispatch({type: 'PUZZLE_GENERATION_ERROR', error}));
+                .then(puzzle => dispatch({type: types.PUZZLE_GENERATION_COMPLETED, payload: {puzzle}}))
+                .catch(error => dispatch({type: types.PUZZLE_GENERATION_ERROR, error}));
         }
     };
 }
