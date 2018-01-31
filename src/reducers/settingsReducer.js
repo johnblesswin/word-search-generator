8a8a5463aa8d51a1fa6230a7e29da83b37be0e0a
Robert Gulewicz <nayael@wp.pl>
2018-01-31 01:05:30 +0100

    Handle SET_GRID_SIZE action

--- src/reducers/settingsReducer.js
+++ src/reducers/settingsReducer.js
@@ -1,15 +1,26 @@
 import { settings as initialState } from '../store/initialState';
 
 export default function settingsReducer(state = initialState, action) {
-  switch (action.type) {
-    case 'SET_GRID_SIZE': // as it says
-    case 'SWITCH_LANGUAGE': // as it says
-    case 'TOGGLE_WORD_CROSSING': // as it says
-    case 'PUZZLE_IS_BEING_GENERATED': // lock edit
-    case 'PUZZLE_GENERATED': // unlock edit, untouch grid settings
-    case 'ERROR_GENERATING_PUZZLE': // unlock edit
+    switch (action.type) {
+        case 'SET_GRID_SIZE': return setGridSize(state, action.payload.size);
+        case 'SWITCH_LANGUAGE': // as it says
+        case 'TOGGLE_WORD_CROSSING': // as it says
+        case 'PUZZLE_IS_BEING_GENERATED': // lock edit
+        case 'PUZZLE_GENERATED': // unlock edit, untouch grid settings
+        case 'ERROR_GENERATING_PUZZLE': // unlock edit
 
-    default:
-      return state;
-  }
+        default:
+        return state;
+    }
+}
+
+function setGridSize(state, size) {
+    return {
+        ...state,
+        touched: true,
+        gridSize: {
+            ...state.gridSize,
+            current: size
+        }
+    };
 }
