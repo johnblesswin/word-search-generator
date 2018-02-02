0153b4862590d188bdd9fae635a8fcc50154ea81
Robert Gulewicz <nayael@wp.pl>
2018-02-02 13:51:06 +0100

    Remove mock data

--- src/modules/WordSearchPuzzle/index.js
+++ src/modules/WordSearchPuzzle/index.js
@@ -1,29 +1,30 @@
 import React from 'react';
 import Board from './Board';
 
-// TO BE REMOVED
-import generateWordSearch from '../../services/generatePuzzle/wordSearchGenerator';
-const charset = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'x', 'q', 'v', 'w', 'y', 'z'];
-const words = ['rainy', 'snow', 'sunny', 'wind', 'chilly', 'freezing', 'skiing', 'chinchilla', 'hamster', 'rabbit', 'honeymoon',
-'magazine', 'newspaper', 'computer', 'jarring', 'petulence', 'comparison', 'mismatching', 'scarcity', 'obesity', 'masterrace', 'serfdom',
-'sorcerer', 'magic', 'wand', 'spell', 'deatheater', 'wizard', 'dreaming', 'planet', 'wolverine', 'collapsing', 'mountains', 'minstrel',
-'galaxy'];
-const sampleData = generateWordSearch(60, words, charset);
-
 class WordSearchPuzzle extends React.Component {
 
-  render() {
-    return (
-      <div>
-        <Board 
-          letters={sampleData.letters}
-          words={sampleData.words}
-          cols={sampleData.boardWidth}
-          highlightWords={true}
-        />
-      </div>
-    );
-  }
+
+    _render() {
+        return (
+            <React.Fragment>
+                {this.props.puzzle}
+            </React.Fragment>
+        );
+    }
+
+    render() {
+        const {puzzle} = this.props;
+        return (
+        <div>
+            <Board 
+                letters={puzzle.letters}
+                words={puzzle.words}
+                cols={puzzle.boardWidth}
+                highlightWords={true}
+            />
+        </div>
+        );
+    }
 
 }
 
