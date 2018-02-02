81ef42201f742fa8fc3f98259e342289bc4d39fb
Robert Gulewicz <nayael@wp.pl>
2018-02-02 02:58:42 +0100

    Fix tests

--- src/__tests__/reducers/puzzleGeneratorReducer.test.js
+++ src/__tests__/reducers/puzzleGeneratorReducer.test.js
@@ -1,4 +1,4 @@
-import { default as reducer } from '../reducers/puzzleGeneratorReducer';
+import { default as reducer } from '../../reducers/puzzleGeneratorReducer';
 import { puzzle as initialState } from '../../store/initialState';
 import * as actions from '../../actions';
 import * as types from '../../actions/types';
@@ -42,12 +42,13 @@ describe('puzzle generator reducer', () => {
         state.generated = null;
         const newPuzzle = 'new puzzle';
         action = {type: types.PUZZLE_GENERATION_COMPLETED, payload: {puzzle: newPuzzle}};
+        state = reducer(state, action);
         expect(state.generated).toBe(newPuzzle);
     });
 
     it('should clear the isPending flag when a new puzzle is ready', () => {
         state.isPending = true;
-        action = {type: types.PUZZLE_GENERATION_COMPLETED};
+        action = {type: types.PUZZLE_GENERATION_COMPLETED, payload: {puzzle: null}};
         state = reducer(state, action);
         expect(state.isPending).toBe(false);
     });
