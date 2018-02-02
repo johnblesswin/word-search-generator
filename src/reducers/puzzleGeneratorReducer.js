3be5192fb2a33fab398d0a0fd40e096d9c13f9c3
Robert Gulewicz <nayael@wp.pl>
2018-02-02 23:04:01 +0100

    Handle all actions

--- src/reducers/puzzleGeneratorReducer.js
+++ src/reducers/puzzleGeneratorReducer.js
@@ -1,26 +1,40 @@
 import { puzzle as initialState } from '../store/initialState';
 
 export default function puzzleGeneratorReducer(state = initialState, action) {
-  switch (action.type) {
-    case 'PUZZLE_IS_BEING_GENERATED':
-      return puzzleIsBeingGenerated(state);
-    case 'PUZZLE_GENERATED':
-      return puzzleGenerated(state, action.payload.puzzle);
-    case 'ERROR_GENERATING_PUZZLE':
-      return errorGeneratingPuzzle(state, action.error);
-    default:
-      return state;
-  }
+    switch (action.type) {
+        case 'PUZZLE_GENERATION_PENDING':
+            return puzzleIsBeingGenerated(state);
+        case 'PUZZLE_GENERATION_COMPLETED':
+            return puzzleGenerated(state, action.payload.puzzle);
+        case 'PUZZLE_GENERATION_ERROR':
+            return errorGeneratingPuzzle(state, action.error);
+        default:
+            return state;
+    }
 }
 
 function puzzleIsBeingGenerated(state) {
-  return state;
+    return {
+        ...state,
+        generated: null,
+        error: false,
+        isPending: true
+    };
 }
 
 function puzzleGenerated(state, puzzle) {
-  return state;
+    return {
+        ...state,
+        generated: puzzle,
+        error: false,
+        isPending: false
+    };
 }
 
 function errorGeneratingPuzzle(state, error) {
-  return state;
+    return {
+        ...state,
+        error: true,
+        isPending: false
+    };
 }
