1f68909c75164606a35e48213cbdb98eddb74cec
Robert Gulewicz <nayael@wp.pl>
2018-02-04 15:57:58 +0100

    Add app title

--- src/modules/Layout/index.js
+++ src/modules/Layout/index.js
@@ -7,25 +7,28 @@ class Layout extends React.Component {
 
   render() {
     return (
-        <div className="layout">
-            <div className="layout__menu">
-                {this.props.menu}
-            </div>
-            <CSSTransitionGroup
-                transitionName="page"
-                transitionEnterTimeout={300}
-                transitionLeaveTimeout={1}
-            >
-                <div key={this.props.pagePath}> {/* a unique key is required for transitions */}
-                    <div className="layout__page-content">
-                        {this.props.pageContent}
-                    </div>
-                    <div className="layout__nav-buttons">
-                        {this.props.navButtons}
-                    </div>
+        <React.Fragment>
+            <div className="layout">
+            <h1 className="app-name">word search printable generator</h1>
+                <div className="layout__menu">
+                    {this.props.menu}
                 </div>
-            </CSSTransitionGroup>
-        </div>
+                <CSSTransitionGroup
+                    transitionName="page"
+                    transitionEnterTimeout={300}
+                    transitionLeaveTimeout={1}
+                >
+                    <div key={this.props.pagePath}> {/* a unique key is required for transitions */}
+                        <div className="layout__page-content">
+                            {this.props.pageContent}
+                        </div>
+                        <div className="layout__nav-buttons">
+                            {this.props.navButtons}
+                        </div>
+                    </div>
+                </CSSTransitionGroup>
+            </div>
+        </React.Fragment>
     );
   }
 }
