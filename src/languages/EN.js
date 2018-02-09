2a2e1628312af364470528f601e280765bd7ecbd
Robert Gulewicz <nayael@wp.pl>
2018-02-09 02:14:42 +0100

    Add warnings & tidy up

--- src/languages/EN.js
+++ src/languages/EN.js
@@ -1,22 +1,31 @@
 export const name = 'English';
 
-export const charset = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'x', 'q', 'v', 'w', 'y', 'z'];
+export const charset = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'r', 's', 't', 'x', 'q', 'u', 'v', 'w', 'y', 'z'];
 
 export const messages = {
 
-  MENU_SETUP_GRID: "Set grid size",
-  MENU_ADD_WORDS: "Add words",
-  MENU_PREVIEW: "Preview",
-  MENU_PRINT: "Print",
-  NAV_BACK: "Back",
-  NAV_NEXT: "Next",
+    /* menu & navigation buttons */
+    MENU_SETUP_GRID: "Set grid size",
+    MENU_ADD_WORDS: "Add words",
+    MENU_PREVIEW: "Preview",
+    MENU_PRINT: "Print",
+    NAV_BACK: "Back",
+    NAV_NEXT: "Next",
 
-  SUBMITTED_WORD_TOO_SHORT: "The word is too short.",
-  SUBMITTED_WORD_TOO_LONG: "The word is too long and won’t fit the board.",
-  SUBMITTED_WORD_HAS_INVALID_CHAR: "Invalid character: ",
-  SUBMITTED_WORD_HAS_INVALID_CHARS: "Invalid characters: ",
-  PROBLEM_WITH_SOME_WORDS: "Some words on the list don’t match the current grid and/or language settings and won’t be included.",
-  WORD_TOO_LONG: "This word is too long.",
-  WORD_HAS_INVALID_CHAR: "Invalid character: ",
-  WORD_HAS_INVALID_CHARS: "Invalid characters: "
+    /* word input warnings */
+    SUBMITTED_WORD_TOO_SHORT: "The word is too short.",
+    SUBMITTED_WORD_TOO_LONG: "The word is too long and won’t fit the board.",
+    SUBMITTED_WORD_HAS_INVALID_CHAR: "Invalid character: ",
+    SUBMITTED_WORD_HAS_INVALID_CHARS: "Invalid characters: ",
+
+    /* word list warnings */
+    WORD_TOO_LONG: "This word is too long.",
+    WORD_HAS_INVALID_CHAR: "Invalid character: ",
+    WORD_HAS_INVALID_CHARS: "Invalid characters: ",
+
+    /* general notifications */
+    PUZZLE_READY: "Your word search puzzle is ready!",
+    PUZZLE_ERROR: "Error generating word search puzzle :(",
+    MAX_WORD_LENGTH_EXCEEDED: "Some words are too long and won't be included. Check the word list.",
+    INVALID_CHARS_FOUND: "Some words have invalid characters and won't be included. Check the word list.",
 };
