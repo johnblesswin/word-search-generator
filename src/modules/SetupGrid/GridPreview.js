868429ed9c03a60b57afaab3bd91c2a8e632d805
Robert Gulewicz <nayael@wp.pl>
2018-02-22 01:15:29 +0100

    Make GridPreview responsive to layout size

--- src/modules/SetupGrid/GridPreview.js
+++ src/modules/SetupGrid/GridPreview.js
@@ -3,6 +3,32 @@ import PropTypes from 'prop-types';
 
 class GridPreview extends React.PureComponent {
 
+    state = {
+        actualContainerSize: 0
+    }
+
+    componentDidMount() {
+        this.calculateDimensions();
+        window.addEventListener("resize", this.calculateDimensions);
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener("resize", this.calculateDimensions);
+    }
+
+    getActualContainerWidth = () => {
+        if (!this.container) return;
+        return this.container.getBoundingClientRect().width;
+    }
+
+    calculateDimensions = () => {
+        const {cols} = this.props,
+        actualWidth = this.getActualContainerWidth();
+        this.setState(state => ({
+            actualContainerSize: this.getActualContainerWidth()
+        }));
+    }
+
     getRows() {
         const {gridSize} = this.props;
 
@@ -15,8 +41,16 @@ class GridPreview extends React.PureComponent {
     }
 
     render() {
+        const size = this.state.actualContainerSize;
+
         return (
-            <table className="grid-preview__grid">
+            <table 
+                className="grid-preview__grid"
+                ref={table => this.container = table}
+                style={{
+                    height: `${size}px`
+                }}
+            >
                 <tbody>
                     {this.getRows()}
                 </tbody>
