f5079ade239a386d52f267f32b803e336ad7fde1
Robert Gulewicz <nayael@wp.pl>
2018-04-28 21:41:28 +0200

    Add print info and button & align items

--- src/modules/Print/index.js
+++ src/modules/Print/index.js
@@ -4,36 +4,59 @@ import * as actions from '../../actions';
 import { connect } from 'react-redux';
 import WordSearchPuzzle from '../WordSearchPuzzle';
 import WordBox from './WordBox';
+import { Button, Icon } from 'antd';
 import './Print.css';
 
 class Print extends React.Component {
 
-  constructor(props) {
-    super(props);
-    this.props.actions.requestPuzzle();
-  }
+    constructor(props) {
+        super(props);
+        this.props.actions.requestPuzzle();
+    }
 
-  render() {
-        if (!this.props.puzzle.generated) {
-            return <p>Generating puzzle...</p>;
-        }
-        return (
-            <div className="printable">
-                <WordSearchPuzzle puzzle={this.props.puzzle.generated}/>
-                <WordBox words={this.props.words} />
-            </div>
-        );
-  }
+    triggerPrint = () => {
+        window.print();
+    }
+
+    printablePuzzle() {
+            if (!this.props.puzzle.generated) return null;
+            return (
+                <div className="printable">
+                    <WordSearchPuzzle puzzle={this.props.puzzle.generated}/>
+                    <WordBox words={this.props.words} />
+                </div>
+            );
+    }
+
+    render() {
+            return (
+                <div className="print">
+                    <Button
+                        type="primary"
+                        icon="printer"
+                        size="large"
+                        onClick={this.triggerPrint}
+                    >
+                        {this.props.lang.PRINT_BUTTON}
+                    </Button>
+                    <p class="print-note">
+                    {this.props.lang.PRINT_NOTE}
+                    </p>
+                    {this.printablePuzzle()}
+                </div>
+            );
+    }
 
 }
 
 function mapStateToProps(state, ownProps) {
-  return {
-    puzzle: state.puzzle,
-    words: (state.puzzle.generated
-        ? state.puzzle.generated.words.map(item => item.word)
-        : [])
-  };
+    return {
+        lang: state.settings.language.messages,
+        puzzle: state.puzzle,
+        words: (state.puzzle.generated
+            ? state.puzzle.generated.words.map(item => item.word)
+            : [])
+    };
 }
 
 function mapDispatchToProps(dispatch) {
