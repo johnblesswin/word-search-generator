9798f3d7e368b78a3d8a0af1a5030a9bbc823e57
Robert Gulewicz <nayael@wp.pl>
2018-05-06 02:22:39 +0200

    Provide store for testing

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { MemoryRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import configureStore from '../store';
 import App from '../App';
+
+const store = configureStore();
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
+    <Provider store={store}>
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>
+    </Provider>,
     div
   );
 });
